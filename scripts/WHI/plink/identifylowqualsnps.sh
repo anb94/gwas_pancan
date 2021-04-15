7ecfef4ef4f97cea9f979854b1b01431ad31d1bc
anb <anb@anbs-MacBook-Pro.local>
2021-04-15 20:19:34 +0100

    Mod plink to preserve master file

--- scripts/WHI/plink/identifylowqualsnps.sh
+++ scripts/WHI/plink/identifylowqualsnps.sh
@@ -13,15 +13,23 @@ WHI_SHARE_aa_cb=${WHI_SHARE}/combined_consentgroups/geno/WHI_SHARE_aa.genotype/
 WHI_SHARE_ha_cb=${WHI_SHARE}/combined_consentgroups/geno/WHI_SHARE_ha.genotype/
 
 for ((i=1; i<=22; i++)); do
-echo "Generating low quality SNPs for African Americans"
-awk '{if ($7 < 0.3) print $1}' ${WHI_SHARE_aa_c1}/SHAREchr${i}aa.info > ${WHI_SHARE_aa_cb}/SHAREchr${i}aa_lq03_snps.txt
-awk '{if ($7 < 0.8) print $1}' ${WHI_SHARE_aa_c1}/SHAREchr${i}aa.info > ${WHI_SHARE_aa_cb}/SHAREchr${i}aa_lq08_snps.txt
+echo "Copying Files and Generating low quality SNPs for African Americans"
+echo "Doing Chromosome number ${i}"
+
+cp ${WHI_SHARE_aa_c1}/SHAREchr${i}aa.info ${WHI_SHARE_aa_cb}/SHAREchr${i}aa.info
+
+awk '{if ($7 < 0.3) print $1}' ${WHI_SHARE_aa_cb}/SHAREchr${i}aa.info > ${WHI_SHARE_aa_cb}/lowqualsnps/SHAREchr${i}aa_lq03_snps.txt
+awk '{if ($7 < 0.8) print $1}' ${WHI_SHARE_aa_cb}/SHAREchr${i}aa.info > ${WHI_SHARE_aa_cb}/lowqualsnps/SHAREchr${i}aa_lq08_snps.txt
 done
 echo "Completed Generating low quality SNPs for African Americans"
 
 for ((i=1; i<=22; i++)); do
-echo "Generating low quality SNPs for Hispanic Americans"
-awk '{if ($7 < 0.3) print $1}' ${WHI_SHARE_ha_c1}/SHAREchr${i}ha.info > ${WHI_SHARE_ha_cb}/SHAREchr${i}ha_lq03_snps.txt
-awk '{if ($7 < 0.8) print $1}' ${WHI_SHARE_ha_c1}/SHAREchr${i}ha.info > ${WHI_SHARE_ha_cb}/SHAREchr${i}ha_lq08_snps.txt
+echo "Copying Files and Generating low quality SNPs for Hispanic Americans"
+echo "Doing Chromosome number ${i}"
+
+cp ${WHI_SHARE_ha_c1}/SHAREchr${i}ha.info ${WHI_SHARE_ha_cb}/SHAREchr${i}ha.info
+
+awk '{if ($7 < 0.3) print $1}' ${WHI_SHARE_ha_cb}/SHAREchr${i}ha.info > ${WHI_SHARE_ha_cb}/lowqualsnps/SHAREchr${i}ha_lq03_snps.txt
+awk '{if ($7 < 0.8) print $1}' ${WHI_SHARE_ha_cb}/SHAREchr${i}ha.info > ${WHI_SHARE_ha_cb}/lowqualsnps/SHAREchr${i}ha_lq08_snps.txt
 done
 echo "Completed Generating low quality SNPs for Hispanic Americans"
