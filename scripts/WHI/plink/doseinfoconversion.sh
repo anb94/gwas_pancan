61d1ddd934e374fd9674821d5cf4ab4f4c710fe1
anb <anb@anbs-MacBook-Pro.local>
2021-04-29 17:50:13 +0100

    change back to home

--- scripts/WHI/plink/doseinfoconversion.sh
+++ scripts/WHI/plink/doseinfoconversion.sh
@@ -2,7 +2,7 @@
 
 # Define environment variables
 ## Define SHARE Root directory:
-WHI_SHARE=$HOME/scratch/dbgap_data/WHI
+WHI_SHARE=$HOME/dbgap_data/WHI
 ## Define combined consent group directories:
 WHI_SHARE_aa_cb=${WHI_SHARE}/combined_consentgroups/geno/WHI_SHARE_aa.genotype
 WHI_SHARE_ha_cb=${WHI_SHARE}/combined_consentgroups/geno/WHI_SHARE_ha.genotype
