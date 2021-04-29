693f24449bc6f5c0ff08a3415c707f544488e58e
anb <anb@anbs-MacBook-Pro.local>
2021-04-29 14:21:56 +0100

    change dir and pick up at chr10aa

--- scripts/WHI/plink/doseinfoconversion.sh
+++ scripts/WHI/plink/doseinfoconversion.sh
@@ -2,24 +2,25 @@
 
 # Define environment variables
 ## Define SHARE Root directory:
-WHI_SHARE=$HOME/dbgap_data/WHI
+WHI_SHARE=$HOME/scratch/dbgap_data/WHI
 ## Define combined consent group directories:
 WHI_SHARE_aa_cb=${WHI_SHARE}/combined_consentgroups/geno/WHI_SHARE_aa.genotype
 WHI_SHARE_ha_cb=${WHI_SHARE}/combined_consentgroups/geno/WHI_SHARE_ha.genotype
 
-# Directory containing dose2plink script (information on this script in README.md file)
-dose2plink_dir=${HOME}/gwas_pancan/scripts/WHI/plink/
+# This still will need to be performed on a machine which has installed dose2plink.c 
+# This can be acquired at https://github.com/anb94/plink-ng
+# Alternatively, a singularity definition file which includes dose2plink.c can be found at ------
 
 
-# Use the dose2plink perl script on African American group:
-for ((i=1; i<=22; i++)); do
+# Use the dose2plink to convert African American group .dose and .info to .pdat and .pfam:
+for ((i=10; i<=22; i++)); do
     echo "Converting .info and .dose for African American Chromosome ${i}"
     dose2plink -m 5000 -dose "${WHI_SHARE_aa_cb}"/SHAREchr"${i}"aa.dose.cb -info "${WHI_SHARE_aa_cb}"/SHAREchr"${i}"aa.info -gz 0 -out "${WHI_SHARE_aa_cb}"/dose2plinkout/SHAREchr"${i}"aa
 done
 
 echo "Completed African American Chromosomes"
 
-# Use the dose2plink perl script on Hispanic American group:
+# Use the dose2plink to convert Hispanic American group .dose and .info to .pdat and .pfam:
 for ((i=1; i<=22; i++)); do
     echo "Converting .info and .dose for Hispanic American Chromosome ${i}"
     dose2plink -m 5000 -dose "${WHI_SHARE_ha_cb}"/SHAREchr"${i}"ha.dose.cb -info "${WHI_SHARE_ha_cb}"/SHAREchr"${i}"ha.info -gz 0 -out "${WHI_SHARE_ha_cb}"/dose2plinkout/SHAREchr"${i}"ha
