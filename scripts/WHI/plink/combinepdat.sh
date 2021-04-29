c96f819fd1080474b8629cd78d60878bea2c27bf
anb <anb@anbs-MacBook-Pro.local>
2021-04-29 23:28:59 +0100

    Add comments to combinepdat.sh

--- scripts/WHI/plink/combinepdat.sh
+++ scripts/WHI/plink/combinepdat.sh
@@ -7,13 +7,17 @@ WHI_SHARE=$HOME/dbgap_data/WHI
 WHI_SHARE_aa_cb=${WHI_SHARE}/combined_consentgroups/geno/WHI_SHARE_aa.genotype/dose2plinkout
 WHI_SHARE_ha_cb=${WHI_SHARE}/combined_consentgroups/geno/WHI_SHARE_ha.genotype/dose2plinkout
 
+# The pdat files for each chromosome need to be combined into a single file.
 
+## African American
+## Add the first file into a new pdat file and then append the rest of the fiels without the header
 cat "${WHI_SHARE_aa_cb}"/SHAREchr1aa.pdat > "${WHI_SHARE_aa_cb}"/SHARE_aa.pdat
 for ((i=2; i<=22; i++)); do
     cat "${WHI_SHARE_aa_cb}"/SHAREchr"${i}"aa.pdat | tail -n +2 >> "${WHI_SHARE_aa_cb}"/SHARE_aa.pdat
 done
 
-
+## Hispanic American
+## Add the first file into a new pdat file and then append the rest of the fiels without the header
 cat "${WHI_SHARE_ha_cb}"/SHAREchr1ha.pdat > "${WHI_SHARE_ha_cb}"/SHARE_ha.pdat
 for ((i=2; i<=22; i++)); do
     cat "${WHI_SHARE_ha_cb}"/SHAREchr"${i}"ha.pdat | tail -n +2 >> "${WHI_SHARE_ha_cb}"/SHARE_ha.pdat
