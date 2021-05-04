9dcdccff718e293148c89f2cc2dfb10d19284f64
anb <anb@anbs-MacBook-Pro.local>
2021-05-04 16:14:47 +0100

    add combinelowqualsnps.sh

--- scripts/WHI/plink/combinelowqualsnps.sh
+++ scripts/WHI/plink/combinelowqualsnps.sh
@@ -27,11 +27,11 @@ done
 ## Append the low qual 0.3 snp files to a combined file
 for ((i=2; i<=22; i++)); do
     echo "Doing SHARE_ha lq03 ${i}"
-    cat "${WHI_SHARE_ha_cb}"/SHAREchr"${i}"aa_lq03_snps.txt >> "${WHI_SHARE_ha_cb}"/SHARE_ha_lq03_snps.txt
+    cat "${WHI_SHARE_ha_cb}"/SHAREchr"${i}"ha_lq03_snps.txt >> "${WHI_SHARE_ha_cb}"/SHARE_ha_lq03_snps.txt
 done
 
 ## Append the low qual 0.8 snp files to a combined file
 for ((i=2; i<=22; i++)); do
     echo "Doing SHARE_ha lq08 ${i}"
-    cat "${WHI_SHARE_ha_cb}"/SHAREchr"${i}"aa_lq08_snps.txt >> "${WHI_SHARE_ha_cb}"/SHARE_ha_lq08_snps.txt
+    cat "${WHI_SHARE_ha_cb}"/SHAREchr"${i}"ha_lq08_snps.txt >> "${WHI_SHARE_ha_cb}"/SHARE_ha_lq08_snps.txt
 done
