65e952befafa817aa71d5a182cb8c7cdb139cc8c
anb <anb@anbs-MacBook-Pro.local>
2021-06-10 18:53:59 +0800

    add importdosage

--- scripts/WHI/plink/plink2-importdosage.sh
+++ scripts/WHI/plink/plink2-importdosage.sh
@@ -10,13 +10,13 @@ WHI_SHARE_ha_cb=${WHI_SHARE}/combined_consentgroups/geno/WHI_SHARE_ha.genotype
 
 
 plink2 --import-dosage "${WHI_SHARE_aa_dose2plink}"/SHARE_aa.pdat \
-	--psam "${WHI_SHARE_aa_cb}"/SHARE_aa.pfam \
+	--psam "${WHI_SHARE_aa_dose2plink}"/SHARE_aa.pfam \
 	--exclude "${WHI_SHARE_aa_cb}"/lowqualsnps/SHARE_aa_lq_all_snps.txt \
 	--make-pgen \
 	--out "${WHI_SHARE_aa_dose2plink}"/WHI_SHARE_aa_temp
 
 plink2 --import-dosage "${WHI_SHARE_ha_dose2plink}"/SHARE_ha.pdat \
-	--psam "${WHI_SHARE_ha_cb}"/SHARE_ha.pfam \
+	--psam "${WHI_SHARE_ha_dose2plink}"/SHARE_ha.pfam \
 	--exclude "${WHI_SHARE_ha_cb}"/lowqualsnps/SHARE_ha_lq_all_snps.txt \
 	--make-pgen \
 	--out "${WHI_SHARE_ha_dose2plink}"/WHI_SHARE_ha_temp
