fbb9fadc84625fe08847f8f4911270f11318be51
anb94 <anb@anbs-MacBook-Pro.local>
2021-07-25 23:29:40 +0800

    chmod new scripts and fix typo

--- scripts/WHI/plink/plink2-qc.sh
+++ scripts/WHI/plink/plink2-qc.sh
@@ -10,30 +10,36 @@ WHI_SHARE_ha_cb=${WHI_SHARE}/combined_consentgroups/geno/WHI_SHARE_ha.genotype/p
 ### Step 1 ### 
 
 # Investigate missingness per individual and per SNP and make histograms.
+echo "Investigating missingness per individuals for african american"
 plink2 --pfile "${WHI_SHARE_aa_cb}"/SHARE_aa_temp_updated --missing
+echo "Investigating missingness per individuals for hispanic american"
 plink2 --pfile "${WHI_SHARE_ha_cb}"/SHARE_ha_temp_updated --missing
 # output: plink.imiss and plink.lmiss, these files show respectively the proportion of missing SNPs per individual and the proportion of missing individuals per SNP.
 
 
 # Generate plots to visualize the missingness results.
-Rscript --no-save hist_miss.R
+#Rscript --no-save hist_miss.R
 
 # Delete SNPs and individuals with high levels of missingness, explanation of this and all following steps can be found in box 1 and table 1 of the article mentioned in the comments of this script.
 # The following two QC commands will not remove any SNPs or individuals. However, it is good practice to start the QC with these non-stringent thresholds.  
 # Delete SNPs with missingness >0.2.
+echo "Delete SNPs with a missingness of more than 0.2"
 plink2 --pfile "${WHI_SHARE_aa_cb}"/WHI_SHARE_aa_temp_updated --geno 0.2 --make-pgen --out "${WHI_SHARE_aa_cb}"/WHI_SHARE_aa_temp_2
 plink2 --pfile "${WHI_SHARE_ha_cb}"/WHI_SHARE_ha_temp_updated --geno 0.2 --make-pgen --out "${WHI_SHARE_ha_cb}"/WHI_SHARE_ha_temp_2
 
 # Delete individuals with missingness >0.2.
+echo "Delete individuals with a missingness of more than 0.2"
 plink2 --pfile "${WHI_SHARE_aa_cb}"/WHI_SHARE_aa_temp_2 --mind 0.2 --make-pgen --out "${WHI_SHARE_aa_cb}"/WHI_SHARE_aa_temp_3
 plink2 --pfile "${WHI_SHARE_ha_cb}"/WHI_SHARE_ha_temp_2 --mind 0.2 --make-pgen --out "${WHI_SHARE_ha_cb}"/WHI_SHARE_ha_temp_3
 
 # Delete SNPs with missingness >0.02.
+echo "Delete SNPs with a missingness of more than 0.02"
 plink2 --pfile "${WHI_SHARE_aa_cb}"/WHI_SHARE_aa_temp_3 --geno 0.02 --make-pgen --out "${WHI_SHARE_aa_cb}"/WHI_SHARE_aa_temp_4
 plink2 --pfile "${WHI_SHARE_ha_cb}"/WHI_SHARE_ha_temp_3 --geno 0.02 --make-pgen --out "${WHI_SHARE_ha_cb}"/WHI_SHARE_ha_temp_4
 
 
 # Delete individuals with missingness >0.02.
+echo "Delete individuals with a missingness of more than 0.02"
 plink2 --pfile "${WHI_SHARE_aa_cb}"/WHI_SHARE_aa_temp_4 --mind 0.02 --make-pgen --out "${WHI_SHARE_aa_cb}"/WHI_SHARE_aa_temp_5
 plink2 --pfile "${WHI_SHARE_ha_cb}"/WHI_SHARE_ha_temp_4 --mind 0.02 --make-pgen --out "${WHI_SHARE_ha_cb}"/WHI_SHARE_ha_temp_5
 
@@ -53,6 +59,7 @@ plink2 --pfile "${WHI_SHARE_ha_cb}"/WHI_SHARE_ha_temp_4 --mind 0.02 --make-pgen 
 #Rscript --no-save MAF_check.R
 
 # Calculate MAFs. Remove all variants with MAF < 0.05 from the current analysis.
+echo "Delete variants with a minor allele frequency of more than 0.05"
 plink2 --pfile "${WHI_SHARE_aa_cb}"/WHI_SHARE_aa_temp_5 --maf 0.05 --make-pgen --out "${WHI_SHARE_aa_cb}"/WHI_SHARE_aa_temp_6
 plink2 --pfile "${WHI_SHARE_ha_cb}"/WHI_SHARE_ha_temp_5 --maf 0.05 --make-pgen --out "${WHI_SHARE_ha_cb}"/WHI_SHARE_ha_temp_6
 
