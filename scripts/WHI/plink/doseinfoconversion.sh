294a89ee72da4d029bc04e5d557269f0e6505bf4
anb94 <anbennett@pm.me>
2021-09-27 21:20:59 +0800

    canary v7

--- scripts/WHI/plink/doseinfoconversion.sh
+++ scripts/WHI/plink/doseinfoconversion.sh
@@ -7,13 +7,13 @@ WHI_SHARE=$HOME/dbgap_data/WHI
 WHI_SHARE_aa_cb=${WHI_SHARE}/combined_consentgroups/geno/WHI_SHARE_aa.genotype
 WHI_SHARE_ha_cb=${WHI_SHARE}/combined_consentgroups/geno/WHI_SHARE_ha.genotype
 
-# This still will need to be performed on a machine which has installed dose2plink.c 
+# This still will need to be performed on a machine which has installed dose2plink.c
 # This can be acquired at https://github.com/anb94/plink-ng
 # Alternatively, a singularity definition file which includes dose2plink.c can be found at https://github.com/anb94/gwas_pancan/blob/master/singularity_images/custom_singularity_images/canary_v4.def
 
 
 # Use the dose2plink to convert African American group .dose and .info to .pdat and .pfam:
-for ((i=10; i<=22; i++)); do
+for ((i=1; i<=22; i++)); do
     echo "Converting .info and .dose for African American Chromosome ${i}"
     dose2plink -m 7000 -dose "${WHI_SHARE_aa_cb}"/SHAREchr"${i}"aa.dose.cb -info "${WHI_SHARE_aa_cb}"/SHAREchr"${i}"aa.info -gz 0 -out "${WHI_SHARE_aa_cb}"/testing-dose2plinkout/SHAREchr"${i}"aa
 done
@@ -28,5 +28,3 @@ for ((i=1; i<=22; i++)); do
 done
 
 echo "Completed Hispanic American Chromosomes"
-
-
